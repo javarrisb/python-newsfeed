d383025ac6d294e54deae00d4f06e97408e483db
Javarris Barnett <javarrisb@gmail.con>
2022-04-10 22:32:37 -0400

    finished models

--- app/models/Post.py
+++ app/models/Post.py
@@ -2,9 +2,9 @@ from datetime import datetime
 from email.policy import default
 from typing import Counter
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime 
-from sqlalchemy.orm import relationship
-
+from .Vote import Vote 
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func 
+from sqlalchemy.orm import relationship, column_property 
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -15,3 +15,8 @@ class Post(Base):
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     user = relationship('User')
+    comments = relationship('Comment', cascade='all,delete')
+    votes = relationship('Vote', cascade='all, delete')
+    vote_count = column_property(
+  select([func.count(Vote.id)]).where(Vote.post_id == id)
+)
